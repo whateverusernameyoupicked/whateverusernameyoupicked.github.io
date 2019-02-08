788880cabbb4258833fbe36f7e9c946ffdaf9a13
whateverusernameyoupicked <47329870+whateverusernameyoupicked@users.noreply.github.com>
2019-02-08 11:03:59 -0600

    bouncing balls

--- cs30-p5js-template-master/sketch.js
+++ cs30-p5js-template-master/sketch.js
@@ -5,11 +5,43 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x, y;
+let xSpeed, ySpeed;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = width/2;
+  y = height/2;
+  xSpeed = random(-3,3);
+  ySpeed = random(-5.5);
+  background(0);
+  noStroke();
 }
 
 function draw() {
-  background(220);
+  //background(220);
+  ellipse(x, y, 25, 25);
+  x += xSpeed;
+  y += ySpeed;
+  fill(random(255),random(255),random(255));
+
+
+  if (x < 0 || x > width) {
+    xSpeed *= -1;
+
+  }
+  if (y < 0 || y > height) {
+    ySpeed *= -1;
+  }
+}
+
+function mousePressed(){
+  fill(255,0,0); 
+  xSpeed = random(-3,3);
+  ySpeed = random(-5.5);
+}
+
+function mouseReleased(){
+  fill(0,255,0);
 }
